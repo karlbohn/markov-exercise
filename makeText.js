5622d365ef3a4a463fe37630fd7e26f2f80672c9
Karl Bohn <karl@McBain>
2024-03-31 11:26:42 -0400

    Updated makeText file to generate text from file or URL, and command line functionality

--- makeText.js
+++ makeText.js
@@ -1,2 +1,48 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+
+function generate(text) {
+    let machine = new markov.MarkovMachine(text);
+    console.log(machine.generate());
+  }
+
+function makeText(path) {
+    fs.readFile(path, "utf8", function x(err, data){
+        if (err) {
+            console.error(`Error reading file: ${path} : ${err}`);
+            process.exit(1)
+        }
+        else {
+            generate(data);
+        }
+    })
+}
+
+async function makeURLText(url){
+    let res;
+    try{
+        res = await axios.get(url);
+    }
+    catch (err){
+        console.error(`Error retrieving url: ${url} : ${error}`);
+        process.exit(1);
+    }
+    generate(res.data)
+}
+
+let [method, path] = process.argv.slice(2);
+
+if (method === 'file'){
+    makeText(path);
+}
+else if (method === 'url'){
+    makeURLText(path);
+}
+else {
+    console.error(`Invalid method: ${method}`)
+    process.exit(1);
+}
